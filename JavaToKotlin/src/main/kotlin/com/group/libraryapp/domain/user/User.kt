50b3ca343426e78a640ba31e4c61d0977c85be7b
w1shope <wishhopelee@gmail.com>
2024-07-08 11:06:42 +0900

    리포지토리 계층 Kotlin으로 변경

--- JavaToKotlin/src/main/kotlin/com/group/libraryapp/domain/user/User.kt
+++ JavaToKotlin/src/main/kotlin/com/group/libraryapp/domain/user/User.kt
@@ -4,6 +4,11 @@ import com.group.libraryapp.domain.book.Book
 import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory
 import javax.persistence.*
 
+/**
+ * JPA Entity는 data class를 지양한다.
+ * data class -> equals, hashcode, toString 구현
+ * User, UserLoanHistory는 1:N관계, equals()를 호출하면 서로 순환 참조
+ */
 @Entity
 class User(
     var name: String,
