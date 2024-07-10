266441e2327f34de07ef42cbaae87f48c4a06780
w1shope <wishhopelee@gmail.com>
2024-07-10 20:16:26 +0900

    spring data jpa -> querydsl

--- JavaToKotlin/src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
+++ JavaToKotlin/src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
@@ -3,12 +3,13 @@ package com.group.libraryapp.service.book
 import com.group.libraryapp.domain.book.Book
 import com.group.libraryapp.domain.book.BookRepository
 import com.group.libraryapp.domain.user.UserRepository
-import com.group.libraryapp.domain.user.loanhistory.UserLoanHistoryRepository
 import com.group.libraryapp.domain.user.loanhistory.UserLoanStatus
 import com.group.libraryapp.dto.book.request.BookLoanRequest
 import com.group.libraryapp.dto.book.request.BookRequest
 import com.group.libraryapp.dto.book.request.BookReturnRequest
 import com.group.libraryapp.dto.book.response.BookStatResponse
+import com.group.libraryapp.repository.book.BookQueryDslRepository
+import com.group.libraryapp.repository.loanhistory.UserLoanHistoryQueryDslRepository
 import com.group.libraryapp.util.fail
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -17,7 +18,8 @@ import org.springframework.transaction.annotation.Transactional
 class BookService(
     private val bookRepository: BookRepository,
     private val userRepository: UserRepository,
-    private val userLoanHistoryRepository: UserLoanHistoryRepository
+    private val bookQueryDslRepository: BookQueryDslRepository,
+    private val userLoanHistoryQueryDslRepository: UserLoanHistoryQueryDslRepository
 ) {
 
     @Transactional
@@ -29,7 +31,7 @@ class BookService(
     @Transactional
     fun loanBook(request: BookLoanRequest) {
         val book = bookRepository.findByName(request.bookName) ?: fail()
-        if (userLoanHistoryRepository.findByBookNameAndStatus(
+        if (userLoanHistoryQueryDslRepository.find(
                 book.name,
                 UserLoanStatus.LOANED
             ) != null
@@ -49,7 +51,7 @@ class BookService(
 
     @Transactional(readOnly = true)
     fun countLoanedBook(): Int {
-        return userLoanHistoryRepository.countByStatus(UserLoanStatus.LOANED).toInt()
+        return userLoanHistoryQueryDslRepository.count(UserLoanStatus.LOANED).toInt()
     }
 
     @Transactional(readOnly = true)
@@ -64,6 +66,6 @@ class BookService(
 //        return bookRepository.findAll()
 //            .groupBy { book -> book.type }
 //            .map { (type, books) -> BookStatResponse(type, books.size) }
-        return bookRepository.getStats()
+        return bookQueryDslRepository.getStats()
     }
 }
